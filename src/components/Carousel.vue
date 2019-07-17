c63ca0c072c01937a43ddaf09b252328bf2ea49e
Alexey Naumov <rocketbuzzz@gmail.com>
2019-07-17 16:14:55 +0300

    Do some simple changes.

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,44 +1,53 @@
 <template>
   <div class="q-pa-md">
-    <q-carousel
-      swipeable
-      animated
-      v-model="slide"
-      :autoplay="autoplay"
-      ref="carousel"
-      infinite
-    >
-      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
-      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
-      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
-      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+     <q-carousel
+        v-model="slide"
+        transition-prev="scale"
+        transition-next="scale"
+        swipeable
+        animated
+        control-color="white"
+        navigation
+        padding
+        arrows
+        height="300px"
+        class="bg-primary text-white shadow-1 rounded-borders"
+      >
+        
 
-      <template v-slot:control>
-        <q-carousel-control
-          position="top-right"
-          :offset="[18, 18]"
-          class="text-white"
-          style="background: rgba(0, 0, 0, .3); padding: 4px 8px 4px 0; border-radius: 4px"
-        >
-          <q-toggle dense dark color="orange" v-model="autoplay" label="Auto Play" />
-        </q-carousel-control>
 
-        <q-carousel-control
-          position="bottom-right"
-          :offset="[18, 18]"
-          class="q-gutter-xs"
+        <q-carousel-slide 
+          class="column no-wrap flex-center"
+          name="style"
         >
-          <q-btn
-            push round dense color="orange" text-color="black" icon="arrow_left"
-            @click="$refs.carousel.previous()"
-          />
-          <q-btn
-            push round dense color="orange" text-color="black" icon="arrow_right"
-            @click="$refs.carousel.next()"
-          />
-        </q-carousel-control>
-      </template>
-    </q-carousel>
+          <q-icon name="style" size="66px" />
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>          
+        </q-carousel-slide>
+
+        <q-carousel-slide name="tv" class="column no-wrap flex-center">
+          <q-icon name="live_tv" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+
+        <q-carousel-slide name="layers" class="column no-wrap flex-center">
+          <q-icon name="layers" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+
+        <q-carousel-slide name="map" class="column no-wrap flex-center">
+          <q-icon name="terrain" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+
+      </q-carousel>    
   </div>
 </template>
 
@@ -47,8 +56,8 @@ export default {
   // name: 'ComponentName',
   data () {
     return {
-      slide: 1,
-      autoplay: false
+      slide: 'style',
+      lorem: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo.'
     }
   }
 }
